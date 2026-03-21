5870c619f6bd0a3d1da1a8eaff9153b6790e2814
Abhishek Maurya <abhishekrmaurya2020@gmail.com>
2026-03-21 16:34:03 +0530

    second commit

--- abstraction.cpp
+++ abstraction.cpp
@@ -1,1 +1,18 @@
-hello guys how are you this my readme file
+#include<iostream>
+#include<string>
+using namespace std;
+class car{
+    public:
+    virtual void startengine()=0;
+    virtual void stopengine()=0;
+    virtual void accelerate()=0;
+    virtual void brake()=0;
+    virtual void changegear(int gear)=0;
+    virtual ~car(){};
+}
+class sportcar:public car{
+    
+}
+int main(){
+
+}
