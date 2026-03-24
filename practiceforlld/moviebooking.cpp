29d231702f635f3e93cb1031cbb3b3897a755444
Abhishek Maurya <abhishekrmaurya2020@gmail.com>
2026-03-25 02:08:54 +0530

    second commit

--- practiceforlld/moviebooking.cpp
+++ practiceforlld/moviebooking.cpp
@@ -22,11 +22,178 @@ class Show{
    this->screen=vector<vector<int>>(r,vector<int>(c,0));
  }
  // find out the continous 
- vector<pair<int,int>>findcontinous()
+ vector<pair<int,int>>findcontinous(int k)
+ {
+    int row=screen.size();
+    int column=screem[0].size();
+    for(int i=0;i<row;i++){
+        int count=0;
+    for(int j=0;j<column;j++)
+    {
+        if(screen[i][j]==0)
+        count++;
+        else
+        count=0;
+        
+        vector<pair<int,int>>result;
+        if(count==k){
+            int start=j-k+1;
+            for(int p=0;p<k;p++)
+            result.push_back({i,start+p});
+           return result;
+        }
+    }
+    }
+ }
+ //find the any position that are free;
+vector<pair<int,int>>findany(int k){
+ int count=0;
+ int row=screen.size();
+ int column=screen[0].size();
+ for(int i=0;i<row;i++){
+    for(int j=0;j<column;j++)
+    {
+       if(screen[i][j]==0){
+        count++;
+        result.push_back({i,j});
+       }
+       if(count==k){
+        return count;
+          
+    }
+ }
+ return {};
+}
+}
+// book the ticket;
+vector<pair<int,int>>bookticket(int k){
+    if(freesheet<k){
+        return {};
+    }
+    auto ans=findcontinous(k);
+    if(ans.empty()){
+        ans=findany(k);
+    }
+    if(ans.size()<k){
+        return {};
+    }
+    for(auto it:ans){
+        screen[it.first][it.second]=1;
+        freesheet--;
+    }
+    return ans;
+}
+//free the booking sheet 
+void freethebookingposition(vector<pair<int,int>>&ans)
+{
+    for(auto it:ans){
+        screen[it.first][it.second]=0;
+        freesheet++;
+    }
+}
+}
+// making the cinema entity;
+class cinema{
+  int cinemaid,cityid;
+  vector<Screen>screen;
+  cinema(int cinemaid,int cityid,int totalscreen,int row,int column)
+  {
+    this->cinemaid=cinemaid;
+    this->cityid=cityid;
+    for(int i=0;i<totalscreen;i++)
+    {
+        screen.push_back(Screen(row,column));
+    }   
+    
+  }
+
+}
+class Ticket
+{
+    string ticketid;
+    int showid;
+    vector<pair<int,int>>seats;
+    bool iscancelled;
+    Ticket(string ticketid,int showid,vector<pair<int,int>>seats)
+    {
+        this->ticketid=ticketid;
+        this->showid=showid;
+        this->seats=seats;
+        this->iscancelled=false;
+    }
+
+}
+// solution to solve this problem;
+class solution{
+    Helper*helper;
+    unordered_map<int,cinema*>mapping_cinama;
+    unordered_map<int,Show*>mapping_show;
+    unordered_map<int,Ticket*>mapping_ticket;
+    unordered_map<int,set<int>>cityToCinemas;
+    unordered_map<int,vector<int>>cinema_to_show;
+    unordered_map<int,vector<int>>moving_to_show;
+
+    public:
+    void init(Helper*h){
+        this->helper=h;
+    }
+    void addcinema(int cinid,int cityid,int total_screen,int row,int column)
+    {
+     cinema*c=new cinema(cinid,cityid,total_screen,row,column);
+     mapping_cinama[cinid]=c;
+     cityToCinemas[cityid].insert(cinid);
+    }
+    void addshow(int showid,int cinemaid,int movieid,int screenindex,int r,int c)
+    {
+     Show*s=new Show(showid,cinemaid,movieid,screenindex,r,c);
+     mapping_show[showid]=s;
+     cinema_to_show[cinemaid].push_back(showid);
+     moving_to_show[movieid].push_back(showid);
+    }
   
+vector<string> bookTicket(String ticketId,
+int showId, int ticketsCount)
+{
+    if(mapping_show.find(showId)==mapping_show.end())
+    {
+        return {};
+    }
+    Show*s=mapping_show[showId];
+    auto booked=s->bookticket(ticketsCount);
+    if(booked.empty()){
+        return {};
+    }
+    vector<string>result;
+    for(auto it:booked){
+        result.push_back(to_string(it.first)+"_"+to_string(it.second));
+    }
+    return result;
+
+}
+ bool cancelticket(string ticketid)
+ {
+   if(mapping_ticket.find(ticketid)==mapping_ticket.end()){
+    return false;
+   }
+ Ticket *t=mapping_ticket[ticketid];
+ bool isticketcancelled=t->iscancelled;
+ if(isticketcancelled){
+    return false;
+ }
+Show*s=mapping_show[t->showid];
+s->freethebookingposition(t->seats);
+t->iscancelled=true;
+return true;
+ 
 }
 
-
-int main(){
+int getfreeseatcount(int showid)
+{
+    if(mapping_show.find(showid)==mapping_show.end()){
+        return -1;
+    }
+    Show*s=mapping_show[showid];
+    return s->freesheet;
+}
 
 }
