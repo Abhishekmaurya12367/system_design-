b00ad7648590dfdeba1018aac39e3347ce15e7f0
Abhishek Maurya <abhishekrmaurya2020@gmail.com>
2026-06-25 17:03:36 +0530

    adaptor_pattern

--- adaptor_designpattern.cpp
+++ adaptor_designpattern.cpp
@@ -31,7 +31,19 @@ XmlDataProviderAdaptor(Xmldataprovider*provider){
 string getJsonData(const string data)override{
     // get xml data from the adaptor
     string xml=xmlprovider-> XmlData(data);
+    //naively parse out <name> and <id> values 
+    size_t StartName=xml.find("<Name>")+6
+    size_t EndtName=xml.find("</Name>");
+    string Name=xml.substr(StartName,EndtName-StartName);
 
+// for the ids;
+size_t Start_id=xml.find("<id>")+4;
+size_t End_id=xml.find("</id>")
+string id=xml.substr(Start_id,End_id-Start_id);
+return "{"\Name":\""+Name+\"",\"id\":"+ id +"}";
+
+
+    
 }
 
 
