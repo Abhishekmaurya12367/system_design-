dfa4e5a3127e9dfa1b9b144a444cc4374fe5b577
Abhishek Maurya <abhishekrmaurya2020@gmail.com>
2026-06-25 15:44:23 +0530

    adaptor_pattern

---
+++ adaptor_designpattern.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+// targated interfacae expected by the client 
+class Report{
+    public:
+    vertual string getJsonData(const string data)=0;
+    vertual ~IReports(){}
+};
+class Xmldataprovider{
+    public:
+    string XmlData(const string& data){
+        size_t sep=data.find(':');
+        string name=data.substr(0,sep);
+        string id=data.substr(sep+1);
+        return "<user>"
+               "<name>"+name+"</name>"
+               "<id>"+id+"</id"
+               "</user>"
+                }
+}
