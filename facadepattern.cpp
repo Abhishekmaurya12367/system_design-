339ddf9ddd8d47c8cc41ff9fb2694aa935eddded
Abhishek Maurya <abhishekrmaurya2020@gmail.com>
2026-06-29 02:50:41 +0530

    added_some_code

--- facadepattern.cpp
+++ facadepattern.cpp
@@ -18,3 +18,35 @@ class cpu{
     cout<<"cpu initialisaation has been started"<<endl;
 }
 }
+class memory{
+    public:
+    void selftest(){
+        cout<<"memory:self test has been passed"<<endl;
+    }
+}
+class hardware{
+    void spinup(){
+        cout<<"harqjfgjbbvb vcbvbvhk"<<endl;
+    }
+}
+class bios{
+    public:
+    void boot(cpu&cp,memory& mem)
+    {
+        cout<<" bios:booting the cpu and memory"<<endl;
+        cp.initialisation();
+        mem.selftest();
+        
+    }
+}
+class operatingsystem{
+    public:
+    void load(){
+        cout<<"operating system:loading into memory"<<endl;
+    }
+}
+
+class computefacade
+{
+
+}
