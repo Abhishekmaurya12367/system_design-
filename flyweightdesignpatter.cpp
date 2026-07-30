9d663b1d69ed3765675771ec44a14da49abc7dc4
Abhishek Maurya <abhishekrmaurya2020@gmail.com>
2026-07-30 17:19:32 +0530

    added_code

--- flyweightdesignpatter.cpp
+++ flyweightdesignpatter.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-it understand by taking example that stroinoid comming where a person shooting the number stronoid depend on the difficulty lavel of
-of the game assume that we have 10 lakh astronoid 3 same and 4 different now to save ram we split it in to the two part first one 
-is intrinsic and second one is extrinsic in the intrinsic it keep all the same type variable that can be reused and in the extrinsic 
-we keep the changing variable  so here we need to make 10 lakh astroinoid only make two
+// it understand by taking example that stroinoid comming where a person shooting the number stronoid depend on the difficulty lavel of
+// of the game assume that we have 10 lakh astronoid 3 same and 4 different now to save ram we split it in to the two part first one 
+// is intrinsic and second one is extrinsic in the intrinsic it keep all the same type variable that can be reused and in the extrinsic 
+// we keep the changing variable  so here we need to make 10 lakh astroinoid only make two
