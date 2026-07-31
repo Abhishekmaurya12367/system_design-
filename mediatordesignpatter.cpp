b74fd325f454c4e36fe78e9145e4bae8b935dc76
Abhishek Maurya <abhishekrmaurya2020@gmail.com>
2026-07-31 13:15:06 +0530

    added_code

---
+++ mediatordesignpatter.cpp
@@ -0,0 +1,4 @@
+// here i am going to discuss about the mediator  before mediator to talk the other person each should have reference of that such as id 
+// object1 want to interact with the object in this scenerio obj1 should have reference of the obj2 vice versa 
+// after introducing the mediator now mediator reference of each object whenever one object want to interact to other object just it call 
+// to the mediator and as well as send message what does it sent to object mediator have reference of each directly it send object
